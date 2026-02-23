6bed86a8b890a5482b99ca85fa992451b6cd7ada
EnricoVBerto <enricovberto@hotmail.com>
2026-02-23 20:54:40 -0300

    erro compilação

--- exe4/main.c
+++ exe4/main.c
@@ -1,52 +1,58 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include <stdio.h>
-const uint BTN_PIN=28;
+
+const uint BTN_PIN = 28;
 const uint LED_PIN_R = 5;
 const uint LED_PIN_ROXO = 8;
 const uint LED_PIN_AZUL = 11;
 const uint LED_PIN_AMARELO = 15;
 
 int main(void) {
-  stdio_init_all();
+    stdio_init_all();
 
-  gpio_init(BTN_PIN);
-  gpio_set_dir(BTN_PIN, GPIO_IN);
-  gpio_pull_up(BTN_PIN);
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
-  gpio_init(LED_PIN_R);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
-  gpio_put(LED_PIN_R, 0);
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
-  gpio_init(LED_PIN_ROXO);
-  gpio_set_dir(LED_PIN_ROXO, GPIO_OUT);
-  gpio_put(LED_PIN_ROXO, 0);
+    gpio_init(LED_PIN_ROXO);
+    gpio_set_dir(LED_PIN_ROXO, GPIO_OUT);
+    gpio_put(LED_PIN_ROXO, 0);
 
-  gpio_init(LED_PIN_AZUL);
-  gpio_set_dir(LED_PIN_AZUL, GPIO_OUT);
-  gpio_put(LED_PIN_AZUL, 0);
+    gpio_init(LED_PIN_AZUL);
+    gpio_set_dir(LED_PIN_AZUL, GPIO_OUT);
+    gpio_put(LED_PIN_AZUL, 0);
 
-  gpio_init(LED_PIN_AMARELO);
-  gpio_set_dir(LED_PIN_AMARELO, GPIO_OUT);
-  gpio_put(LED_PIN_AMARELO, 0);
+    gpio_init(LED_PIN_AMARELO);
+    gpio_set_dir(LED_PIN_AMARELO, GPIO_OUT);
+    gpio_put(LED_PIN_AMARELO, 0);
 
-  sleep_ms(110);
+    sleep_ms(110);
 
-  while (true) {
-    if(!gpio_get(BTN_PIN)) {
-        gpio_put(LED_PIN_R, 1);
-        sleep_ms(300);
-        gpio_put(LED_PIN_R, 0);
-        gpio_put(LED_PIN_ROXO, 1);
-        sleep_ms(300);
-        gpio_put(LED_PIN_ROXO, 0);
-        gpio_put(LED_PIN_AZUL, 1);
-        sleep_ms(300);
-        gpio_put(LED_PIN_AZUL, 0);
-        gpio_put(LED_PIN_AMARELO, 1);
-        sleep_ms(300);
-        gpio_put(LED_PIN_AMARELO, 0);
-        while(!gpio_get(BTN_PIN)) {
-        };
-  }
+    while (true) {
+        if (!gpio_get(BTN_PIN)) {
+            gpio_put(LED_PIN_R, 1);
+            sleep_ms(300);
+            gpio_put(LED_PIN_R, 0);
+
+            gpio_put(LED_PIN_ROXO, 1);
+            sleep_ms(300);
+            gpio_put(LED_PIN_ROXO, 0);
+
+            gpio_put(LED_PIN_AZUL, 1);
+            sleep_ms(300);
+            gpio_put(LED_PIN_AZUL, 0);
+
+            gpio_put(LED_PIN_AMARELO, 1);
+            sleep_ms(300);
+            gpio_put(LED_PIN_AMARELO, 0);
+
+            while (!gpio_get(BTN_PIN)) {
+            }
+        }
+    }
 }
